cc9e463de7c3f45d75f15feb305caee59eb1231f
HrebeniakDmytro <hrebeniak.dmytro@gmail.com>
2024-11-05 18:43:19 +0200

    questions for bot(Install with finel.py)

---
+++ questions_db.py
@@ -0,0 +1,182 @@
+questions_db = {
+    1: {
+        "question": "Яка планета відома своїми кільцями?",
+        "options": ["A) Юпітер", "B) Сатурн", "C) Нептун", "D) Марс"],
+        "correct": "B",
+        "URL-question": "https://web-shturman.com.ua/wp-content/uploads/2024/02/tsikavi-fakty-pro-planety-giganty.jpg"
+    },
+    2: {
+        "question": "Яка наукова дисципліна вивчає поведінку живих організмів?",
+        "options": ["A) Фізика", "B) Хімія", "C) Біологія", "D) Астрономія"],
+        "correct": "C",
+        "URL-question": "https://ua.kursoviks.com.ua/thumb/2/dfNlwe8fo1UzltF5gAzT6g/580r450/d/distsiplina-osnovi-naukovikh-doslidzhen.jpg"
+    },
+    3: {
+        "question": "Хто написав 'Гаррі Поттера'?",
+        "options": ["A) Касандра Кларк", "B) Джоан Роулінг", "C) Стівен Кінг", "D) Сесілія Ахерн"],
+        "correct": "B",
+        "URL-question": "https://images.ctfassets.net/usf1vwtuqyxm/6fjvdemYmo7kJ2V5P5Xqhc/1fc2b0b396123f891e858069e00d0d2b/hp-f6-harry-at-great-hall-table-web-fact-file-image.jpg?h=416&w=600&fit=pad"
+    },
+    4: {
+        "question": "Яка країна є батьківщиною олімпійських ігор?",
+        "options": ["A) Рим", "B) Греція", "C) Кіпр", "D) Єгипет"],
+        "correct": "B",
+        "URL-question": "https://www.bsmu.edu.ua/wp-content/uploads/2019/03/5942.jpg"
+    },
+    5: {
+        "question": "Яка частина людського тіла найбільше містить кісток?",
+        "options": ["A) Рука", "B) Нога", "C) Хребет", "D) Череп"],
+        "correct": "C",
+        "URL-question": "https://st.depositphotos.com/2363887/2563/i/450/depositphotos_25637409-stock-photo-male-human-skeleton-four-views.jpg"
+    },
+    6: {
+        "question": "Яка планета є найбільшою в нашій Сонячній системі?",
+        "options": ["A) Венера", "B) Земля", "C) Юпітер", "D) Сатурн"],
+        "correct": "C",
+        "URL-question": "https://web-shturman.com.ua/wp-content/uploads/2024/02/tsikavi-fakty-pro-planety-giganty.jpg"
+    },
+    7: {
+        "question": "Який газ є основним компонентом земної атмосфери?",
+        "options": ["A) Кисень", "B) Азот", "C) Вуглекислий газ", "D) Аргон"],
+        "correct": "B",
+        "URL-question": "https://cdn.meteoprog.net/thumbnails/newsweather/cropr_1200x784/big_599913.jpg?1639734184"
+    },
+    8: {
+        "question": "Хто написав роман '1984'?",
+        "options": ["A) Френсіс Скотт Фіцджеральд", "B) Джордж Оруелл", "C) Ернест Хемінгуей", "D) Марк Твен"],
+        "correct": "B",
+        "URL-question": "https://static.yakaboo.ua/media/cloudflare/product/webp/600x840/2/6/26_1_329.jpg"
+    },
+    9: {
+        "question": "Яка ріка є найдовшою у світі?",
+        "options": ["A) Ніл", "B) Амазонка", "C) Янцзи", "D) Міссісіпі"],
+        "correct": "A",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQktX_0IOhVUbH2UONbhDyWnpfVXBWt9wmREQ&s"
+    },
+    10: {
+        "question": "Скільки днів у лютому в невисокосному році?",
+        "options": ["A) 28", "B) 29", "C) 30", "D) 31"],
+        "correct": "A",
+        "URL-question": "https://24tv.ua/resources/photos/news/202406/2585759.jpg?v=1719576637000"
+    },
+    11: {
+        "question": "Яка країна славиться своїми кенгуру?",
+        "options": ["A) Канада", "B) Австралія", "C) Бразилія", "D) Індія"],
+        "correct": "B",
+        "URL-question": "https://petsexotic.com.ua/assets/gallery/102/red__medium.jpg"
+    },
+    12: {
+        "question": "Яка відома вежа розташована в Парижі?",
+        "options": ["A) Біг-Бен", "B) Вежа Клейтон", "C) Ейфелева вежа", "D) Вежа Бурдж Халіфа"],
+        "correct": "C",
+        "URL-question": "https://iprosvita.com/wp-content/uploads/2019/10/1-1.gif"
+    },
+    13: {
+        "question": "Кого вважають 'батьком' сучасної фізики?",
+        "options": ["A) Ісаак Ньютон", "B) Альберт Ейнштейн", "C) Галілео Галілей", "D) Нільс Бор"],
+        "correct": "B",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJcUmMbTprp6UB-2_lX9OMeKjbMdTvUcRthg&s"
+    },
+    14: {
+        "question": "Яка країна є найбільшим виробником кави у світі?",
+        "options": ["A) Ефіопія", "B) Колумбія", "C) Бразилія", "D) В'єтнам"],
+        "correct": "C",
+        "URL-question": "https://sunrisecoffee.ua/content/images/46/krainy-vyrobnyky-kavy-69761740945648.jpg"
+    },
+    15: {
+        "question": "Яка з наступних хвороб викликана вірусом?",
+        "options": ["A) Діабет", "B) Грип", "C) Астма", "D) Гіпертонія"],
+        "correct": "B",
+        "URL-question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Rotavirus_Reconstruction.jpg/800px-Rotavirus_Reconstruction.jpg"
+    },
+    16: {
+        "question": "У якому році відбулася перша висадка людини на Місяць?",
+        "options": ["A) 1965", "B) 1969", "C) 1971", "D) 1975"],
+        "correct": "B",
+        "URL-question": "https://zbruc.eu/sites/default/files/images/2019/07/z1563638465a14i.jpg"
+    },
+    17: {
+        "question": "Яка тварина є символом США?",
+        "options": ["A) Лев", "B) Орел", "C) Ведмідь", "D) Слон"],
+        "correct": "B",
+        "URL-question": "https://s3.animalia.bio/animals/photos/full/1.25x1/bald-eagle-1.webp?id=02553193f7fcf192437f9a74dd9daf4f"
+    },
+    18: {
+        "question": "Хто створив теорію відносності?",
+        "options": ["A) Нікола Тесла", "B) Альберт Ейнштейн", "C) Стівен Гокінг", "D) Річард Фейнман"],
+        "correct": "B",
+        "URL-question": "https://futuro.in.ua/images/Einsteins-Theory-of-General-Relativity-was-Ignored-for-Three-Decades-750x300.jpg"
+    },
+    19: {
+        "question": "Яка найвища гора у світі?",
+        "options": ["A) К2", "B) Гімалаї", "C) Еверест", "D) Монблан"],
+        "correct": "C",
+        "URL-question": "https://cdn.mapme.club/images/4877/48772-samaya-vysokaya-gora-v-mire.jpg"
+    },
+    20: {
+        "question": "Який континент є найбільшим за площею?",
+        "options": ["A) Європа", "B) Африка", "C) Азія", "D) Південна Америка"],
+        "correct": "C",
+        "URL-question": "https://tse-tsikavo.com.ua/wp-content/uploads/2024/10/tsikavi-fakty-pro-yevraziyu.jpg"
+    },
+    21: {
+        "question": "Яка валюта використовується в Японії?",
+        "options": ["A) Долар", "B) Євро", "C) Йена", "D) Фунт"],
+        "correct": "C",
+        "URL-question": "https://upload.wikimedia.org/wikipedia/commons/7/78/JPY_Banknotes.png"
+    },
+    22: {
+        "question": "Яка релігія є найпоширенішою у світі?",
+        "options": ["A) Християнство", "B) Іслам", "C) Буддизм", "D) Індуїзм"],
+        "correct": "A",
+        "URL-question": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Prevailing_religious_population_by_country_percentage.svg/400px-Prevailing_religious_population_by_country_percentage.svg.png"
+    },
+    23: {
+        "question": "Який метал є рідкісним і найдорожчим на ринку?",
+        "options": ["A) Золото", "B) Срібло", "C) Платина", "D) Паладій"],
+        "correct": "D",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiMidNM90pFI3_8PHXuysogavKfPIxwJWaKw&s"
+    },
+    24: {
+        "question": "Яка країна відома своєю пірамідою Хеопса?",
+        "options": ["A) Іран", "B) Єгипет", "C) Ірак", "D) Греція"],
+        "correct": "B",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1kLMGMuElGLtbkCEjlcLxE7M9jx4uP1eEdA&s"
+    },
+    25: {
+        "question": "Який винахід пов'язаний з ім'ям Александра Граема Белла?",
+        "options": ["A) Телевізор", "B) Телефон", "C) Комп'ютер", "D) Радіо"],
+        "correct": "B",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtBtfNNokpJfxh8PeIpYEq-yU-Vu6rA2Xf2Q&s"
+    },
+    26: {
+        "question": "Який основний компонент сонячного світла?",
+        "options": ["A) Кисень", "B) Вода", "C) Світло", "D) Тепло"],
+        "correct": "C",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXo4bpDaeQd2YpugKmNVhFAFi8f9DApW1pmA&s"
+    },
+    27: {
+        "question": "Який місто є столицею Франції?",
+        "options": ["A) Лондон", "B) Париж", "C) Мадрид", "D) Рим"],
+        "correct": "B",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr2gBwJr4ChI5VKFYOyI2ycwpwd8CrwA_5TA&s"
+    },
+    28: {
+        "question": "Яка з цих технологій дозволяє користувачам спілкуватися в реальному часі через інтернет?",
+        "options": ["A) SMS", "B) Email", "C) Відеозв'язок", "D) Сигнали диму"],
+        "correct": "C",
+        "URL-question": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOZJz0Hhuo648xEwsb79GmE-Z8AAjJd5XaCQ&s"
+    },
+    29: {
+        "question": "Яка найбільша пустеля на планеті?",
+        "options": ["A) Сахара", "B) Гобі", "C) Арктична пустеля", "D) Пустеля Калахарі"],
+        "correct": "A",
+        "URL-question": "https://expedicia.org/wp-content/uploads/2023/01/ALsBprYaRjG55XGoNjEPVP.jpg"
+    },
+    30: {
+        "question": "Яка соціальна мережа була заснована Марком Цукербергом?",
+        "options": ["A) Twitter", "B) Instagram", "C) Facebook", "D) LinkedIn"],
+        "correct": "C",
+        "URL-question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Mark_Zuckerberg_em_setembro_de_2014.jpg/250px-Mark_Zuckerberg_em_setembro_de_2014.jpg"
+    }
+}
